75d690c6cd120aecd618dd441a42b8113f33fd85
Samuel Nyarko <samuelnyarko003@gmail.com>
2025-02-10 05:49:27 +0000

    Stories Section Complete

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -1,5 +1,10 @@
+import 'package:chatapphg/helpers.dart';
 import 'package:chatapphg/theme.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../widgets/widgets.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
@@ -18,16 +23,80 @@ class _Stories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: SizedBox(
+        height: 145,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, top: 8.0, bottom: 16.0,),
+              child: Text(
+                'Stories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 15,
+                  color: AppColors.textFaded,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 7,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    final faker = Faker();
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 60,
+                        child: _StoryCard(
+                          storyData: StoryData(
+                            name: faker.person.name(),
+                            url: Helpers.randomPictureUrl(),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  const _StoryCard({
+    Key? key,
+    required this.storyData,
+  }) : super(key: key);
+
+  final StoryData storyData;
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          'Stories',
-          style: TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 15,
-            color: AppColors.textFaded,
+        Avatar.medium(url: storyData.url),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: Text(
+              storyData.name,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 11,
+                letterSpacing: 0.3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        )
+        ),
       ],
     );
   }
